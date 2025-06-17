c0ae91c11eceeebe175e44e16c8a86bbfd0166b0
tishacapps0519 <cappstisha@gmail.com>
2025-06-16 20:37:41 -0400

    Syntax Sugar Exercise

--- SyntaxAndSyntaxSugarExercise/Program.cs
+++ SyntaxAndSyntaxSugarExercise/Program.cs
@@ -4,7 +4,17 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var answer = 10;
+            var response = answer < 9 ? $"{answer} is less than nine" :$" {answer} is greater than or equal to nine" ;
+            Console.WriteLine(response);
+            //if (answer < 9) 
+            //{
+            //    response = answer + " is less than nine";
+            //}
+            //else
+            //{
+            //response = answer + " is greater than or equal to nine";
+            //}
         }
     }
 }
